cbd7e9108278401b0f4c40af55627eb6d26e4a4b
fernicortlo <a01026613@tec.mx>
2023-03-09 11:54:29 -0600

    added javascript file

---
+++ desarrollo web/FRONT/js/class_activity.js
@@ -0,0 +1,7 @@
+/*
+    Ma. Fernanda Cortés Lozano - A01026613
+    Actividad en clase: Javascript
+*/
+
+/* Escribe una función que encuentre el primer carácter de un cadena de texto que no se
+repite. Prueba tu función con: 'abacddbec'*/
