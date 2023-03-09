7afedc3b2cccd48f54df055cb3df852e3aef8395
fernicortlo <a01026613@tec.mx>
2023-03-09 12:50:29 -0600

    finished first form activity

--- desarrollo web/FRONT/js/class_activity.js
+++ desarrollo web/FRONT/js/class_activity.js
@@ -5,3 +5,22 @@
 
 /* Escribe una función que encuentre el primer carácter de un cadena de texto que no se
 repite. Prueba tu función con: 'abacddbec'*/
+
+function NoRepeat(str){
+
+let frequency = {};
+
+for (let i=0; i<str.lenght; i++){
+    if(frequency[str[i]]){
+        frequency[str[i]]++;
+    } else {
+        frequency[str[i]] = 1;
+    }
+
+    console.log(frequency);
+    return null;
+}
+}
+
+let result=NoRepeat("abacddbec");
+console.log(result);
