039ec316bf325ea0018f4a5b59b5c2679336897c
fernicortlo <a01026613@tec.mx>
2023-03-13 12:30:00 -0600

    js

--- desarrollo web/FRONT/js/class_activity.js
+++ desarrollo web/FRONT/js/class_activity.js
@@ -227,6 +227,7 @@ function sortStrings(strings) {
   const sortedArray = sortStrings(unsortedArray);
   console.log(sortedArray);
 
+
 let numbers = [1,2,3,4,5]
 /*12. Escribe una función que tome una lista de números
 y devuelva la mediana y la moda. */
@@ -246,11 +247,11 @@ function mostFrequentString(list) {
       // If the string doesn't exist yet, add it with a frequency of 1
       if (!stringFrequency[str]) {
         stringFrequency[str] = 1;
-        console.log(stringFrequency[str]);
+        stringFrequency[str];
       } else {
         // Otherwise, increment its frequency
         stringFrequency[str]++;
-        console.log(stringFrequency[str]);
+        stringFrequency[str];
       }
   
       // Check if it's the most frequent string so far
@@ -267,3 +268,44 @@ function mostFrequentString(list) {
   const frequentString = mostFrequentString(stringList);
   console.log(frequentString);
 
+//14. Escribe una función que tome un número y devuelva verdadero si es una potencia de dos, falso de lo contrario.
+function PowerofTwo(n){
+    //Check if the given number is a positive value
+    if(n>0){
+        //While loop where the number is divided by two if it's an even number
+        while(n % 2==0){
+            n= n/2;
+        }
+    }
+    //At the end of the while loop, if the result is =1, it is a power of 2
+    if(n==1)
+        return true;
+    else
+        return false;   
+}
+let n=64;
+if (PowerofTwo(n)==true)
+    console.log("True \n");
+else
+    console.log("False \n");
+
+
+function Desencing (Mylist){
+    // Iterate through the array 
+        for (let i = 0; i < Mylist.length; i++) {
+        //Iterate through the array starting one number after i
+            for (let j = i + 1; j < Mylist.length; j++) {
+            //Create an instance if the value j is bigger than i
+            if (Mylist[j] > Mylist[i]) {
+            //Change the  positions of the values
+                //Create a tempral variable for the value swap to be possible
+                temp = Mylist[i];
+                Mylist[i] = Mylist[j];
+                Mylist[j] = temp;
+            }
+        }
+    }
+}
+let Mylist =[1,23,99,33,12];
+Desencing(Mylist);
+console.log(Mylist);
