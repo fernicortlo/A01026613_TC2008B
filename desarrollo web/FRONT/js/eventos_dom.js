bdca5a5c66e884e9f10c40716ee8e7168f08f590
fernicortlo <a01026613@tec.mx>
2023-03-27 14:10:51 -0600

    dom start

---
+++ desarrollo web/FRONT/js/eventos_dom.js
@@ -0,0 +1,10 @@
+// q: ¿Qué es el DOM?
+// r: El DOM es la representación de un documento HTML en forma de árbol de nodos.
+
+// q: ¿Qué es un nodo?
+// r: Un nodo es un elemento del árbol DOM, por ejemplo, un elemento HTML, un texto, un comentario, etc.
+
+//q: ¿Cómo se accede a un elemento del DOM?
+//r: Se accede a un elemento del DOM a través de su ID, por ejemplo, document.getElementById("id_del_elemento")
+
+//q: ¿Cómo se modifica un elemento del DOM?
