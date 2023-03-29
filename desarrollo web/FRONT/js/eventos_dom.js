de0373e3e1fee9a8f90962ae994896d900379192
fernicortlo <a01026613@tec.mx>
2023-03-28 18:13:23 -0600

    theory questions  done

--- desarrollo web/FRONT/js/eventos_dom.js
+++ desarrollo web/FRONT/js/eventos_dom.js
@@ -8,3 +8,19 @@
 //r: Se accede a un elemento del DOM a través de su ID, por ejemplo, document.getElementById("id_del_elemento")
 
 //q: ¿Cómo se modifica un elemento del DOM?
+//r: Se modifica un elemento del DOM a través de su ID, por ejemplo, document.getElementById("id_del_elemento").innerHTML = "Nuevo texto"
+
+//q: ¿Cuál es la diferencia entre getElementby* y querySelector*?
+//r: getElementby* devuelve un elemento, querySelector* devuelve una lista de elementos
+
+//q: ¿Qué es un evento?
+//r: Un evento es una acción que se produce en un elemento del DOM, por ejemplo, un click, un cambio de valor, etc.
+
+//q: ¿Qué es un listener?
+//r: Un listener es una función que se ejecuta cuando se produce un evento en un elemento del DOM
+
+//q: ¿Qué es un callback?
+//r: Un callback es una función que se pasa como parámetro a otra función
+
+//q: ¿Qué es un objeto de evento?
+//r: Un objeto de evento es un objeto que se pasa como parámetro a una función callback
